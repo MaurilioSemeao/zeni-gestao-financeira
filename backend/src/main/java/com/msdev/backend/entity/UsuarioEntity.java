caf548a9f0caaa05f9c76a699aa016306cc04152
maurilio <maurilio.ssemeao@gmail.com>
2025-10-17 00:46:11 -0300

    feat: Implementa a entidade Usuario no backend

---
+++ backend/src/main/java/com/msdev/backend/entity/UsuarioEntity.java
@@ -0,0 +1,92 @@
+package com.msdev.backend.entity;
+
+
+import com.msdev.backend.enums.TipoUsuario;
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "usuario")
+public class UsuarioEntity {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String senhaCriptografada;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private TipoUsuario tipoUsuario;
+
+    public UsuarioEntity() {
+    }
+
+    public UsuarioEntity(String nome, String email, String senhaCriptografada, TipoUsuario tipoUsuario) {
+        this.nome = nome;
+        this.email = email;
+        this.senhaCriptografada = senhaCriptografada;
+        this.tipoUsuario = tipoUsuario;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenhaCriptografada() {
+        return senhaCriptografada;
+    }
+
+    public void setSenhaCriptografada(String senhaCriptografada) {
+        this.senhaCriptografada = senhaCriptografada;
+    }
+
+    public TipoUsuario getTipoUsuario() {
+        return tipoUsuario;
+    }
+
+    public void setTipoUsuario(TipoUsuario tipoUsuario) {
+        this.tipoUsuario = tipoUsuario;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        UsuarioEntity that = (UsuarioEntity) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
